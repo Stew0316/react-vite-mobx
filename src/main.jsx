3993f41796bbd491ad5045efff826d745970dff5
lin <185975315@qq.com>
2023-06-09 17:55:29 +0800

    feat:初始化添加路由

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'antd/dist/reset.css';
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './router/index'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
