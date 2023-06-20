64ac87a017c6ff9c728c544488f07cefea40e2ae
lin <185975315@qq.com>
2023-06-20 17:53:49 +0800

    feat:修改others

--- src/views/others/Open.jsx
+++ src/views/others/Open.jsx
@@ -1,27 +1,33 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { useNavigate } from "react-router"
 function Open(props) {
   const togRef = useRef()
+  const navi = useNavigate()
+  const [opend, setOpend] = useState(false)
+  function menuClick() {
+    setOpend(!opend)
+  }
   useEffect(() => {
-    
+
   })
   return (
     <div className={`${props.className}`}>
-      <nav className="top-right">
-        <a className="disc l1">
-          <div>Messages</div>
-        </a>
-        <a className="disc l2">
-          <div>Photos</div>
-        </a>
-        <a className="disc l3">
+      <nav className={`top-right ${opend ? 'open' : ''}`}>
+        <div className="disc l1" onClick={() => navi('/others')}>
+          <div>Clock</div>
+        </div>
+        <div className="disc l2" onClick={() => navi('/others/tree')}>
+          <div>Tree</div>
+        </div>
+        <div className="disc l3">
           <div>Profile</div>
-        </a>
-        <a className="disc l4">
+        </div>
+        <div className="disc l4">
           <div>Likes</div>
-        </a>
-        <a className="disc l5 toggle" ref={togRef}>
+        </div>
+        <div className="disc l5 toggle" ref={togRef} onClick={menuClick}>
           Menu
-        </a>
+        </div>
       </nav>
     </div>
   )
