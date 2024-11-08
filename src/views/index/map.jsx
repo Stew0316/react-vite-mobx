1ff23c9f894bbf8ef33e2465c6f6af0195783210
linjinxiang <185975315@qq.com>
2024-11-08 17:15:00 +0800

    feat: 添加地图经纬度

--- src/views/index/map.jsx
+++ src/views/index/map.jsx
@@ -1,23 +1,37 @@
 import 'leaflet/dist/leaflet.css'
 import leaflet from 'leaflet'
 import { useEffect } from 'react'
+import chinaJson from './china.json'
+import pic from '/fac.jpg'
+function init() {
+  const canvasRenderer = L.canvas();
+  const map = L.map('map', {
+    center: L.latLng(28.431, 119.008),
+    zoom: 4,
+    preferCanvas: true
+  });
+  var canvasLayer = L.layerGroup().addTo(map);
+  // 这里需要在env里自己配一个maptoken，来自于mapbox的token
+  const key = import.meta.env.VITE_MAP_TOKEN;
+  L.tileLayer(`https://api.mapbox.com/styles/v1/185975315/clv9cnd4d00ol01rj8tcadqm0/tiles/256/{z}/{x}/{y}@2x?access_token=${key}`, {
+      minZoom: 4,
+      // zoom: 14,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  const jsonLayer = L.geoJSON(chinaJson)
+  jsonLayer.addTo(canvasLayer);
+}
+
 function Map () {
+  
   useEffect(() => {
-    const map = L.map('map', {
-      center: L.latLng(25.431, 119.008),
-      zoom: 12,
-    });
-    // 这里需要在env里自己配一个maptoken，来自于mapbox的token
-    const key = import.meta.env.VITE_MAP_TOKEN;
-    L.tileLayer(`https://api.mapbox.com/styles/v1/185975315/clv9cnd4d00ol01rj8tcadqm0/tiles/256/{z}/{x}/{y}@2x?access_token=${key}`, {
-        maxZoom: 22,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    init()
   }, [])
   return (
     <div id='map' style={{
-      height: '100%'
-    }}>map</div>
+      height: '100%',
+      position: 'relative',
+    }}></div>
   )
 }
 
